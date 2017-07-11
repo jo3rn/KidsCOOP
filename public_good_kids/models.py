4a9b503028130ffc7574ba02f664a0bc8ccaf302
koesti31 <koesti31@users.noreply.github.com>
2017-07-11 12:11:12 +0200

    public good skeleton

---
+++ public_good_kids/models.py
@@ -0,0 +1,39 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+author = 'Jörn'
+
+doc = """
+this is a public good game for kids in elementary school
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'pgk'
+    players_per_group = 4
+    num_rounds = 1
+
+    endowment = c(2)
+    efficiency_factor = 2
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
+
+    def set_payoffs(self):
+        self.total_contribution = sum([p.contribution for p in self.get_players()])
+        self.individual_share = self.total_contribution * Constants.efficiency_factor / Constants.players_per_group
+        for p in self.get_players():
+            p.payoff = Constants.endowment - p.contribution + self.individual_share
+
+
+class Player(BasePlayer):
+    contribution = models.CurrencyField(min=0, max=Constants.endowment)
